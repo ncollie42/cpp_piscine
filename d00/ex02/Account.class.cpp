7f3f94631d530dc4be11e78ea794d46d0587d679
Nico <ncollie42@gmail.com>
2019-07-16 12:53:06 -0700

    Started ex02

---
+++ d00/ex02/Account.class.cpp
@@ -0,0 +1,26 @@
+#include "Account.class.hpp"
+
+static int Account::getNbAccounts ( void )
+{
+    return _nbAccounts;
+}
+
+static int	getTotalAmount( void )
+{
+
+}
+
+static int	getNbDeposits( void )
+{
+
+}
+
+static int	getNbWithdrawals( void )
+{
+
+}
+
+static void	displayAccountsInfos( void )
+{
+
+}
