0ba204b7f7d023cdade5f49647bf813223a9a7e8
Nico <ncollie42@gmail.com>
2019-07-15 21:46:11 -0700

    d00

---
+++ d00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int n = 1;
+
+    if (argc != 1)
+        while (n < argc)
+        {
+            std::string tmp = argv[n++];
+            for (int n = 0; n < tmp.length(); n++)
+                std::cout << (char)toupper(tmp[n]);
+        }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
+}
