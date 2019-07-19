a3a6dc353a2af8b7e0003e610f32d589908881af
Nico <ncollie42@gmail.com>
2019-07-18 22:53:43 -0700

    Ex01
    fixed point to floats

--- d02/ex01/Fixed.class.cpp
+++ d02/ex01/Fixed.class.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Fixed.class.hpp"
+#include <cmath>
 
 Fixed::Fixed( void )
 {
@@ -12,11 +13,35 @@ Fixed::~Fixed( void )
     std::cout << "Destructor called" << std::endl;
 }
 
+Fixed::Fixed(const int inum)
+{
+    std::cout << "int constructor called" << std::endl;
+    _fixed_point_value = inum << _fractional_bits;
+}
+
+Fixed::Fixed(const float fnum)
+{
+    std::cout << "float constructor called" << std::endl;
+    _fixed_point_value = (int)(roundf(fnum * (1 << _fractional_bits)));
+}
+
 Fixed::Fixed(const Fixed& src)
 {
     std::cout << "Copy constructor called" << std::endl;
     *this = src;
 }
+
+float Fixed::toFloat (void) const
+{
+    float tmp;
+    tmp = (_fixed_point_value / (float)(1 << _fractional_bits));
+    return tmp;
+}
+
+int Fixed::toInt(void) const
+{
+    return (int)(_fixed_point_value >> _fractional_bits);
+} 
 
 Fixed   &Fixed::operator=(Fixed const &rhs)
 {
@@ -25,9 +50,14 @@ Fixed   &Fixed::operator=(Fixed const &rhs)
     return (*this);
 }
 
+std::ostream & operator << (std::ostream &out, const Fixed &rhs)
+{
+    out << rhs.toFloat();
+    return out;
+}
+
 int     Fixed::getRawBits( void ) const
 {
-    std::cout << "getRawbits function called" << std::endl;
     return _fixed_point_value;
 }
 
--- d02/ex01/Fixed.class.hpp
+++ d02/ex01/Fixed.class.hpp
@@ -1,6 +1,7 @@
 #ifndef FIXED_CLASS_H
 #define FIXED_CLASS_H
 
+
 class Fixed
 {
     public:
@@ -10,15 +11,16 @@ class Fixed
         Fixed(const Fixed &src);
         ~Fixed( void );
         Fixed & operator = (Fixed const & rhs);
-        std::ostream & operator << (Fixed const & rhs);
         
         float toFloat (void) const;
         int toInt(void) const;
         int getRawBits( void ) const;
         void setRawBits ( int const raw);
     private:
-        int _fixed_point_value;
-        static const int _fractional_bits = 8;
+        int _fixed_point_value; //raw, 
+        static const int _fractional_bits = 8; //exponent value, higher = more precision 
 };
 
+std::ostream & operator << (std::ostream &out, const Fixed &rhs);
 #endif
+        
