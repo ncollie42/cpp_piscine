b0d5131a0239ddd76bcb3296f6d022bb3556ed6e
Nico <ncollie42@gmail.com>
2019-07-24 20:34:36 -0700

    ex02

---
+++ d07/ex02/Array.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncollie <ncollie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/24 18:03:10 by ncollie           #+#    #+#             */
+/*   Updated: 2019/07/24 20:33:20 by ncollie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+#include <exception>
+
+template <typename T>
+class Array
+{
+public:
+	Array(void) : _size(0), _arr(nullptr) {}
+	Array(unsigned int size) : _size(size){
+		_arr = new T[size]();
+	}
+	~Array(void) { delete [] _arr;}
+	Array(Array const &src){ *this = src;}
+	Array &operator=(Array const &rhs) {
+		_size = rhs._size;
+		if (_arr )
+		{
+			delete [] _arr;
+		}
+		_arr = new T[_size];
+		for (unsigned int n = 0; n < _size ; n++)
+		{
+			_arr[n] = rhs._arr[n];
+		}
+		return *this;
+	}
+	T& operator[](unsigned int index)
+	{
+		if (index >= _size) {
+			throw Array::IndexException();
+		}
+		return _arr[index];
+	}
+	struct IndexException : public std::exception {
+		const char* what() const throw()
+		{
+			return "Index Out Of Bounds";
+		}
+	};
+	unsigned int size( void ) const {return _size;}
+	//Add your public methods below here
+private:
+	//Add your private methods
+	unsigned int _size;
+	T *_arr;
+	//Add your private attributes below here
+};
+
+#endif
+
---
+++ d07/ex02/main.cpp
@@ -0,0 +1,70 @@
+#include "Array.hpp"
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+int main(void)
+{
+    std::cout << std::setprecision(1) << std::fixed;
+    Array<int> tmp0(8);
+    Array<int> tmp1;
+    Array<float> tmp2(4);
+    Array<char> tmp3(10);
+    Array<std::string> tmp5(10);
+    tmp5[2] = "This";
+    try
+    {
+        std::cout << tmp0[4] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    try
+    {
+        std::cout << tmp1[4] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    tmp1 = tmp0;
+    try
+    {
+        std::cout << tmp1[4] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    try
+    {
+        std::cout << tmp2[2] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    for (unsigned int n = 0; n < 10; n++)
+    {
+        tmp3[n] = 42;
+    }
+    try
+    {
+        std::cout << tmp3[3] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    try
+    {
+        std::cout << tmp5[0] << std::endl;
+        std::cout << tmp5[2] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+}
